8a9eaffdac43360abb3b5a33f09302b3fa7659ab
Ibrohim <ibrokhim.tjk@icloud.com>
2025-10-22 22:27:08 +0500

    ...

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ backgroundColor: "#ffffff" }} 
       >
         {children}
       </body>
